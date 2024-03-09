5a0cee8b24638ab7a2eb22f29940db94025f516f
Kotchapun Roekcharoen <kxttpw.j@gmail.com>
2024-03-10 01:04:28 +0700

    save

--- login.php
+++ login.php
@@ -1,30 +1,31 @@
+<?php include('server.php') ?>
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+  <title>Registration system PHP and MySQL</title>
+  <link rel="stylesheet" type="text/css" href="style.css">
 </head>
 <body>
-    <header>
-        <h1> Log in </h1>
-    </header>
-
-    <div class="header">
-        <h2>login</h2>
-    </div>
-
-    <form action="logindb.php" method="post">
-    
-        <div class="input-group">
-        <input type="text" name="username" placeholder="Username" require>
-        </div>
-
-        <div class="input-group">
-        <input type="password" name="password" placeholder="Password" require>
-        </div>
-
-        <div class="input-group">
-        <button type="submit" name="login" class="btn">login</button>
-        </div>
-    </form>
+  <div class="header">
+  	<h2>Login</h2>
+  </div>
+	 
+  <form method="post" action="login.php">
+  	<?php include('errors.php'); ?>
+  	<div class="input-group">
+  		<label>Username</label>
+  		<input type="text" name="username" >
+  	</div>
+  	<div class="input-group">
+  		<label>Password</label>
+  		<input type="password" name="password">
+  	</div>
+  	<div class="input-group">
+  		<button type="submit" class="btn" name="login_user">Login</button>
+  	</div>
+  	<p>
+  		Not yet a member? <a href="register.php">Sign up</a>
+  	</p>
+  </form>
+</body>
+</html>
